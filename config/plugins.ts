0d6b90200f25a2c4e14bf38d6216d2a361712bd5
Sahrane_Guassemi <sahranedev@gmail.com>
2025-04-24 16:18:53 +0200

    mise en place de la config pour l'infrastructure de l'auth de strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "localhost",
+        port: 1025,
+        secure: false,
+      },
+      settings: {
+        defaultFrom: "no-reply@local.dev",
+        defaultReplyTo: "support@local.dev",
+      },
+    },
+  },
+});

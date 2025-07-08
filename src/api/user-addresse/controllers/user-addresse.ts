9044ae2aa35e5814377ec162822d1df2ca21e686
Sahrane_Guassemi <sahranedev@gmail.com>
2025-07-08 15:13:00 +0200

    ajout d'un controller personnalisé pour les user-adresses

--- src/api/user-addresse/controllers/user-addresse.ts
+++ src/api/user-addresse/controllers/user-addresse.ts
@@ -5,5 +5,33 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController(
-  "api::user-addresse.user-addresse"
+  "api::user-addresse.user-addresse",
+  ({ strapi }) => ({
+    // Hérite de toutes les actions par défaut
+    async find(ctx) {
+      // Appel de l'action par défaut
+      const { data, meta } = await super.find(ctx);
+      return { data, meta };
+    },
+
+    async findOne(ctx) {
+      const { data } = await super.findOne(ctx);
+      return { data };
+    },
+
+    async create(ctx) {
+      const { data } = await super.create(ctx);
+      return { data };
+    },
+
+    async update(ctx) {
+      const { data } = await super.update(ctx);
+      return { data };
+    },
+
+    async delete(ctx) {
+      const { data } = await super.delete(ctx);
+      return { data };
+    },
+  })
 );
